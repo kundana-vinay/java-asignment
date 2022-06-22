15dea3bcaff41486c75c4b0e4641f9a50f91b3c3
praveen <praveen@LAPTOP-L1AADILB>
2022-06-22 19:30:35 +0530

    java

---
+++ java-asignments/src/java/sample.java
@@ -0,0 +1,20 @@
+package java;
+
+
+
+public class sample {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sample sm = new sample();
+		sm.method();
+
+	}
+
+	public void method() {
+		// TODO Auto-generated method stub
+		int a = 1;
+		System.out.println(a);
+	}
+
+
+}
